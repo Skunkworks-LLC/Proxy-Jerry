49266d98d3337060adca3739ade1a37bb61d9bef
Jerry Wu <wuj828@gmail.com>
2018-09-18 15:50:02 -0700

    Express server loading page and displaying data retrieved from each module's server.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 
-// app.use((req, res, next) => {
-//    res.header(“Access-Control-Allow-Origin”, “*”);
-//    res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
-//    next();
-// });
+app.use(express.static(__dirname + '/../public'));
 
-app.get('', (req, res) => {
+app.get('/projects', (req, res) => {
   console.log('received get request for proxy server');
   res.status(200).send();
 })
